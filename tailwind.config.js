23d8dea62c5bf23cf2f815cbb20b2eae5b9ab9c7
XinYun2020 <xinyunzhang2016@gmail.com>
2022-10-02 21:36:51 +1100

    wtih darkMode

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,7 @@
+// tailwind.config.js
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  darkMode: "class", // I want darkMode based on a class
   content: [
     "./pages/**/*.{js,ts,jsx,tsx}",
     "./components/**/*.{js,ts,jsx,tsx}",
