67cccd4e60aab539bd61cd491b76a648248dc0be
XinYun2020 <xinyunzhang2016@gmail.com>
2022-10-04 13:54:24 +1100

    Add facebook authentication

--- pages/auth/login.js
+++ pages/auth/login.js
@@ -1,16 +1,22 @@
 import { FcGoogle } from "react-icons/fc";
 import { AiFillFacebook } from "react-icons/ai";
-import { GoogleAuthProvider, signInWithPopup } from "firebase/auth";
+import {
+  GoogleAuthProvider,
+  signInWithPopup,
+  FacebookAuthProvider,
+  updateProfile,
+} from "firebase/auth";
 import { useAuthState } from "react-firebase-hooks/auth";
 import { auth } from "../../utils/firebase";
 import { useRouter } from "next/router";
+import { useEffect } from "react";
 
 export default function Login() {
-  /* SIGN IN WITH GOOGLE */
-  const googleProvider = new GoogleAuthProvider();
   const [user, loading] = useAuthState(auth);
   const route = useRouter();
 
+  /* SIGN IN WITH GOOGLE */
+  const googleProvider = new GoogleAuthProvider();
   const GoogleLogin = async () => {
     try {
       const result = await signInWithPopup(auth, googleProvider);
@@ -20,6 +26,32 @@ export default function Login() {
       console.log(error);
     }
   };
+
+  /* SIGN IN WITH FACEBOOK */
+  const fbProvider = new FacebookAuthProvider();
+  const FacebookLogin = async () => {
+    try {
+      const result = await signInWithPopup(auth, fbProvider);
+      const credantial = await FacebookAuthProvider.credentialFromResult(
+        result
+      );
+      const token = credantial.accessToken; // access token for the login
+      let photoUrl = result.user.photoURL + "?height=500&access_token=" + token; // height: 500 & access_token=token (add token to URL)
+      await updateProfile(auth.currentUser, { photoURL: photoUrl }); // update modify name etc...
+
+      console.log(result);
+      route.push("/dashboard");
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  /* IF LOGGED IN REDIREACT TO DASHBOARD  */
+  useEffect(() => {
+    if (user) route.push("/dashboard");
+
+    console.log("login");
+  }, [user]);
 
   return (
     <>
@@ -36,7 +68,10 @@ export default function Login() {
             <FcGoogle className=" text-2xl" />
             Sign in with Google
           </button>
-          <button className=" text-white bg-gray-700 p-4 w-full font-medium rounded-lg flex align-middle gap-2">
+          <button
+            onClick={FacebookLogin}
+            lassName=" text-white bg-gray-700 p-4 w-full font-medium rounded-lg flex align-middle gap-2"
+          >
             <AiFillFacebook className=" text-2xl text-blue-400" />
             Sign in with Facebook
           </button>
