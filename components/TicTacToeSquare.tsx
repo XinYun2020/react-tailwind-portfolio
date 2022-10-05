2fd06dc79d1229b9e9844784ddc5fd931a594c73
XinYun2020 <xinyunzhang2016@gmail.com>
2022-10-05 17:16:49 +1100

    Add tictactoe game

--- components/TicTacToeSquare.tsx
+++ components/TicTacToeSquare.tsx
@@ -2,13 +2,31 @@ import React from "react";
 import { ReactNode } from "react";
 
 interface TicTacToeSquareProps {
-  children: ReactNode;
+  index: number;
+  onClick(e: any): void;
+  player?: string;
 }
 
-export function TicTacToeSquare({ children }: TicTacToeSquareProps) {
+export default function TicTacToeSquare({
+  index,
+  onClick,
+  player,
+}: TicTacToeSquareProps) {
+  const scale = player ? "scale-100" : "scale-0";
+  const textColor = player === "X" ? "text-yellow-200" : "text-fuchsia-300";
+  const hoverStyle = "transition duration-500 hover:scale-105 transform";
   return (
-    <div className="h-36 border-solid border-4 border-slate-200 font-display text-7xl text-center flex justify-center items-center cursor-pointer">
-      {children}
+    <div
+      data-cell-index={index}
+      className={`h-36 border-solid border-4 border-slate-200 font-display text-7xl text-center flex justify-center items-center cursor-pointer ${hoverStyle}`}
+      {...{ onClick }}
+    >
+      <span
+        data-cell-index={index}
+        className={`transform transition-all duration-150 ease-out ${scale} ${textColor}`}
+      >
+        {player}
+      </span>
     </div>
   );
 }
