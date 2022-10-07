9a45976c9b0beb182a3a5cfc8e954c71716fad71
XinYun2020 <xinyunzhang2016@gmail.com>
2022-10-07 13:19:31 +1100

    tailwind.config.js with color themes

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,15 +1,22 @@
 module.exports = {
-  "stories": [
-    "../stories/**/*.stories.mdx",
-    "../stories/**/*.stories.@(js|jsx|ts|tsx)"
-  ],
-  "addons": [
+  stories: ["../src/**/*.stories.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
+  // "`stories`": ['../src/components/**/*.stories.@(js|jsx|ts|tsx)'],
+  addons: [
     "@storybook/addon-links",
     "@storybook/addon-essentials",
-    "@storybook/addon-interactions"
+    "@storybook/addon-interactions",
+    "@storybook/preset-typescript",
+    "@storybook/addon-actions",
+    "@storybook/preset-scss",
+    "@storybook/addon-coverage",
+    "@storybook/addon-a11y", //👈 The a11y addon goes here
+    "@storybook/addon-jest",
   ],
-  "framework": "@storybook/react",
-  "core": {
-    "builder": "@storybook/builder-webpack5"
-  }
-}
+  framework: "@storybook/react",
+  features: {
+    interactionsDebugger: true,
+  },
+  core: {
+    builder: "webpack5",
+  },
+};
--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,3 +1,28 @@
+import '../styles/index.scss';
+import * as NextImage from "next/image";
+
+import React from "react";
+import { addDecorator } from "@storybook/react";
+import { MemoryRouter } from "react-router";
+import { withTests } from '@storybook/addon-jest';
+import results from '../.jest-test-results.json';
+
+
+
+addDecorator(story => <MemoryRouter initialEntries={['/']}>{story()}</MemoryRouter>);
+
+const OriginalNextImage = NextImage.default;
+
+Object.defineProperty(NextImage, "default", {
+  configurable: true,
+  value: (props) => (
+      <OriginalNextImage
+          {...props}
+          unoptimized
+      />
+  ),
+});
+
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
   controls: {
@@ -7,3 +32,9 @@ export const parameters = {
     },
   },
 }
+
+export const decorators = [
+  withTests({
+    results,
+  }),
+];
