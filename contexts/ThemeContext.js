2be6ae38e1370b02e270440c2ba4778f8aed49cb
XinYun2020 <xinyunzhang2016@gmail.com>
2022-10-19 18:30:46 +1100

    check localStorage theme setting

--- contexts/ThemeContext.js
+++ contexts/ThemeContext.js
@@ -1,16 +1,49 @@
 import React, { useContext, useState } from "react";
 import { BsFillMoonStarsFill } from "react-icons/bs";
 
+const getInitialTheme = () => {
+  if (typeof window !== "undefined" && window.localStorage) {
+    const storedPrefs = window.localStorage.getItem("current-theme");
+    if (typeof storedPrefs === "string") {
+      return storedPrefs;
+    }
+    if (window.matchMedia("(prefers-color-scheme: dark)").matches) {
+      return "dark";
+    }
+  }
+  return "light";
+};
+
 export const ThemeContext = React.createContext();
 
 export function useTheme() {
   return useContext(ThemeContext);
 }
-export default function ThemeProvider({ children }) {
-  const [darkMode, setDarkMode] = useState(true);
+export default function ThemeProvider({ initialTheme, children }) {
+  const [darkMode, setDarkMode] = useState(getInitialTheme);
+
+  const checkTheme = (existing) => {
+    const root = window.document.documentElement;
+    const isDark = existing === "dark";
+
+    root.classList.remove(isDark ? "light" : "dark");
+    root.classList.add(existing);
+
+    localStorage.setItem("current-theme", existing);
+  };
+
+  if (initialTheme) {
+    checkTheme(initialTheme);
+  }
+
+  React.useEffect(() => {
+    checkTheme(darkMode);
+  }, [darkMode]);
+
+  //   const { theme, setTheme } = React.useContext(ThemeContext);
 
   return (
-    <ThemeContext.Provider value={darkMode}>
+    <ThemeContext.Provider value={(darkMode, setDarkMode)}>
       <div className={`${darkMode ? "dark" : ""} `}>
         <div
           className="font-plex px-14 min-h-screen
